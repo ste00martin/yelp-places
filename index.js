3f5ab23fec12c03d7595010eb8ff1245fa4c1785
Stefan Martin <ste00martin@gmail.com>
2018-07-26 13:44:41 -0700

    adding in more yelp fields

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ var Papa = require('papaparse')
 var fileSystem = require('fs');
 const got = require('got');
 const _ = require('lodash')
+const flatten = require('flat')
 
-var txtFile = "activeclusters.csv";
-var fileString = fileSystem.readFileSync(txtFile,'utf8');
+const INPUT_FILE = "Unique.csv";
+// const INPUT_FILE = "testdata.csv";
 
-const YELP_LIMIT = 2 // must be less than  or equal to 50
-const YELP_RADIUS = 1000 // must be less than or equal to 4000
+const YELP_LIMIT = 50 // must be less than  or equal to 50
+const YELP_RADIUS = 40000 // must be less than or equal to 40000
 const YELP_AUTH_TOKEN = process.env.YELP_TOKEN
-const OUTPUT_CSV_FILE_NAME = 'output2.csv'
+const OUTPUT_CSV_FILE_NAME = 'fullYelp.csv'
+// const CATEGORIES = 'Grocery,Gas Stations'
+
+
+var fileString = fileSystem.readFileSync(INPUT_FILE,'utf8');
+
 
 async function getPlaces (latitude, longitude) {
     try {
@@ -30,15 +36,16 @@ async function getPlaces (latitude, longitude) {
 
 async function getYelpWrapper(latitude, longitude) {
   const yelpRequest = await getPlaces(latitude,longitude)
-  const simplified = yelpRequest.businesses.filter(business => !business.is_closed).map(business => {
-    let newBusinessData = _.pick(business, ['distance', 'name', 'alias', 'categories', 'price'])
 
-    newBusinessData.categories = newBusinessData.categories.map(category => category.title)
+  const simplified = yelpRequest.businesses.filter(business => ( business && !business.is_closed)).map(business => {
+    const categories = business.categories.map(category => category.title)
+
+    const subsetBusiness = flatten(_.omit(business, ['transactions', 'categories', 'distance']))
     return {
-      'business name': newBusinessData.name,
-      'business distance meters': newBusinessData.distance,
-      'business categories': _.join(newBusinessData.categories, '~'),
-      'business price': newBusinessData.price,
+      ...subsetBusiness,
+      'distance (meters)': business.distance,
+      // categories,
+      'categories': _.join(categories, '~'),
     }
   })
   return simplified
@@ -48,24 +55,42 @@ const saveParsedData = (neighbors) => {
   var outputCsv = Papa.unparse(neighbors,{
     header: true,
   })
-
+  console.log('output:', outputCsv)
   fileSystem.writeFileSync(OUTPUT_CSV_FILE_NAME, outputCsv, 'utf8')
 }
 
 async function parseComplete (results) {
   let newData = []
   let data = results.data
+
   for (i = 0; i < data.length; i++) {
     let row = data[i]
-    var latitude = row.Latitude
-    var longitude = row.Longitude
-    let yelpData = await getYelpWrapper(latitude,longitude)
-    yelpData.forEach(business => {
-      let stationAndBusinessInfo =  {...row, ...business}
-      newData.push(stationAndBusinessInfo)
-      console.log('new data:' , stationAndBusinessInfo)
-    });
+    let stationLatitude = row['Geolocation (Latitude)']
+    let stationLongitude = row['Geolocation (Longitude)']
+    let stationLIN = row.LIN
+    let stationSalesforceID = row['Location (LIN): ID']
+    let siteAccountName = row['Site Account: Account Name']
+
+    if( stationLatitude && stationLongitude ) {
+      let yelpData = await getYelpWrapper(stationLatitude,stationLongitude)
+
+      yelpData.forEach(business => {
+        let stationAndBusinessInfo = {
+          stationLatitude,
+          stationLongitude,
+          stationLIN,
+          stationSalesforceID,
+          siteAccountName,
+          ...business
+        }
+        newData.push(stationAndBusinessInfo)
+        console.log('new data:' , stationAndBusinessInfo)
+      });
+    }
   }
+
+  // fileSystem.writeFile('./data.json', JSON.stringify(newData, null, 2) , 'utf-8');
+
   saveParsedData(newData)
 }
 
